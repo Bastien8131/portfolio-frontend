f43dd30191b9d15375d9c96b326caa3e64ec74a0
Bastien Bories <94696044+Bastien8131@users.noreply.github.com>
2025-03-17 14:52:35 +0100

    fix: prismjs ajouts des couleur pour les partie code des .md
    création d'un fichier prism-languages.ts qui contient les langues de codage à importer pour la colorisation

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ import 'prismjs/components/prism-javascript';
 import 'prismjs/plugins/line-numbers/prism-line-numbers';
 import 'prismjs/themes/prism-okaidia.css';
 import 'prismjs/plugins/line-numbers/prism-line-numbers.css';
+import './app/core/config/prism-languages';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
