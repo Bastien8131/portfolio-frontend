c101a54c405f04da38f4618aeac25162b76d5de7
Bastien Bories <94696044+Bastien8131@users.noreply.github.com>
2025-03-17 14:41:10 +0100

    feat: installation prismjs, emoji-toolkit & katex
    prismjs :
    
    Ajout de la coloration syntaxique pour les blocs de code
    Configuration des langages typescript et javascript
    Intégration du plugin line-numbers
    Utilisation du thème okaidia
    
    emoji-toolkit :
    
    Support des emojis dans le markdown
    Installation du package emoji-toolkit
    
    katex :
    
    Support des formules mathématiques
    Configuration des styles dans angular.json
    Installation des types pour l'autocomplétion

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,12 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
+import 'prismjs';
+import 'prismjs/components/prism-typescript';
+import 'prismjs/components/prism-javascript';
+import 'prismjs/plugins/line-numbers/prism-line-numbers';
+import 'prismjs/themes/prism-okaidia.css';
+import 'prismjs/plugins/line-numbers/prism-line-numbers.css';
+
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
