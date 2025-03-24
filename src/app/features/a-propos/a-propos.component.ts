1084d6dcf2110e4d8b4bf9fda9a7c326b83d618a
Bastien Bories <94696044+Bastien8131@users.noreply.github.com>
2025-03-24 18:22:24 +0100

    feat: installation de ng2-pdf-viewer & affichage de pdf

--- src/app/features/a-propos/a-propos.component.ts
+++ src/app/features/a-propos/a-propos.component.ts
@@ -4,33 +4,47 @@ import {StrapiFile} from '../../core/models/strapi/file.model';
 import {FilesStrapiService} from '../../core/services/strapi/files.strapi.service';
 import {ProfileStrapiService} from '../../core/services/strapi/profile.strapi.service';
 import {NgIf} from '@angular/common';
+import {MarkdownComponent} from 'ngx-markdown';
+import {PdfViewerModule} from 'ng2-pdf-viewer';
+import {environment} from '../../core/environments/environment';
+import {MatDialog} from '@angular/material/dialog';
+import {DialogPdfComponent} from '../../shared/components/dialog-pdf/dialog-pdf.component';
 
 @Component({
   selector: 'app-a-propos',
   imports: [
-    NgIf
+    MarkdownComponent,
+    PdfViewerModule
   ],
   templateUrl: './a-propos.component.html',
   styleUrl: './a-propos.component.scss'
 })
 export class AProposComponent implements OnInit {
   profile!: Profile;
-  cv?: StrapiFile;
+  url = '';
+  apiUrl = environment.strapiUrl;
 
   constructor(
     private filesStrapiService: FilesStrapiService,
     private profileService: ProfileStrapiService,
+    public dialog: MatDialog
   ) {
   }
 
   ngOnInit() {
     this.profileService.profile$.subscribe(profile => {
       this.profile = profile;
-      this.cv = {
-        ...profile.cv?.data.attributes,
-        id: profile.cv?.data.id
-      }
+      this.url = `${this.apiUrl}${profile.cv?.data.attributes.url}`;
+      // this.cv = {
+      //   ...profile.cv?.data.attributes,
+      //   id: profile.cv?.data.id
+      // }
     });
   }
 
+  openPDF() {
+    let dialogRef = this.dialog.open(DialogPdfComponent, {
+      data: { url: this.url }
+    });
+  }
 }
