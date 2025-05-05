f6243d28cccacd333757f82245b1d47b7106ff00
Bastien Bories <94696044+Bastien8131@users.noreply.github.com>
2025-05-05 21:50:02 +0200

    fix: formatage de la date via pipe

--- src/app/shared/components/dialog-article/dialog-article.component.ts
+++ src/app/shared/components/dialog-article/dialog-article.component.ts
@@ -2,11 +2,13 @@ import {Component, Inject, OnInit} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
 import {Article} from '../../../core/models/strapi/collectionType/article.model';
 import {MarkdownComponent} from 'ngx-markdown';
+import {DatePipe} from '@angular/common';
 
 @Component({
   selector: 'app-dialog-article',
   imports: [
-    MarkdownComponent
+    MarkdownComponent,
+    DatePipe
   ],
   templateUrl: './dialog-article.component.html',
   styleUrl: './dialog-article.component.scss'
@@ -24,14 +26,5 @@ export class DialogArticleComponent implements OnInit {
   ngOnInit() {
     console.log(this.data);
     console.log('test', this.article);
-  }
-
-  // La fonction n'est pas idéal, crée une autre dans un services qui prend en paramètre un datetime et un format. Et renvoie une date au format souhaité
-  getYear(datetime: string) {
-    if (!datetime) {
-      return '';
-    }
-    const date = new Date(datetime);
-    return date.getFullYear().toString();
   }
 }
