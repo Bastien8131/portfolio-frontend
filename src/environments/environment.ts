7d24c8fb7ae3546aebdd0db5b38630890bc31a44
Bastien Bories <94696044+Bastien8131@users.noreply.github.com>
2025-07-08 10:51:22 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   strapiUrl: 'http://bastienbories.androcode.fr:5501',
-  strapiApiToken: '90f1346e2f5378c72bfd8a7938a0aa54bda66128f8dc7ad2497c5e82fb1df6da888726d8eeb923a85ad65244960a5180dc5fde3177f19eac1b74cf3bc9e45e0ba5d9e7d4f004d1a29bc7c0cfc5eca6b8dc8b06a89da837a8c2fe0575138e0e4c95ea59b30526d40bf9880faca0eaed835b37c594835a8a019dc3f31edc362dfd',
+  strapiApiToken: '188def09ccb3f5682a6cca21fd3bcb88b3c79a7ded748e76784656a2d6fb7710ea7a3c3ad21b27e15373578cf412a7335f9e22f8bd5745a3e8881dd818ba3a0e8062a2d88f6e5279ea9319a2866da09052761390f9b33800e21f928506dbaf70bad148082393401e6d8b5cd536e3f38be23a1ab26564ae4f51669f2725cb2dad',
   emailJS:{
     serviceID: 'service_iil9o1l',
     templateID: 'template_uv9b6dj',
