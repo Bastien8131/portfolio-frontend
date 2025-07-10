b9109c2efc6bc9dca1b8fba75d932434786bdfba
Bastien Bories <94696044+Bastien8131@users.noreply.github.com>
2025-07-10 13:57:01 +0200

    fix: url strapi https

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  strapiUrl: 'http://bastienbories.androcode.fr:5501',
+  strapiUrl: 'https://api.bastienbories.androcode.fr',
   strapiApiToken: '188def09ccb3f5682a6cca21fd3bcb88b3c79a7ded748e76784656a2d6fb7710ea7a3c3ad21b27e15373578cf412a7335f9e22f8bd5745a3e8881dd818ba3a0e8062a2d88f6e5279ea9319a2866da09052761390f9b33800e21f928506dbaf70bad148082393401e6d8b5cd536e3f38be23a1ab26564ae4f51669f2725cb2dad',
   emailJS:{
     serviceID: 'service_iil9o1l',
