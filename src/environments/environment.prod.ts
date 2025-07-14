908b1469fb01a784827be8b9a415c82af151359f
Bastien Bories <94696044+Bastien8131@users.noreply.github.com>
2025-07-14 19:05:19 +0200

    fix: edit environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,10 @@
 export const environment = {
   production: true,
-  strapiUrl: 'https://votre-strapi-production.com',
-  strapiApiToken: 'votre-token-api-production'
+  strapiUrl: 'https://api.bastienbories.androcode.fr',
+  strapiApiToken: 'votre-token-api-production', // Remplacer plus tard par le token API de production quand l'environnement sera prêt et sécurisé
+  emailJS:{
+    serviceID: 'service_iil9o1l',
+    templateID: 'template_uv9b6dj',
+    publicKey: 'GbFN0AhXihDg7cDTX'
+  }
 };
